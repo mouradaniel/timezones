7fcc018924562bd18e2ef2c3e1e4e051e3a9260e
Daniel Moura <daniel-moura@outlook.com>
2022-12-22 18:56:22 -0300

    feat: add currenthour and minute

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+
 export function Resume() {
+  const [today, setToday] = useState(null);
+  const [currentHour, setCurrentHour] = useState(null);
+  const [currentMinute, setCurrentMinute] = useState(null);
+
+  useEffect(() => {
+    setToday(new Date());
+
+    setInterval(() => {
+      setToday(new Date());
+    }, 1000 * 60);
+  }, [])
+  
+  useEffect(() => {
+    if (today) {
+      setCurrentHour(today.getHours());
+      setCurrentMinute(today.getMinutes());
+    }
+  }, [today])
+
   return (
     <div>
-      <p>18:00</p>
+      <p>{currentHour}:{currentMinute}</p>
       <span>your local time</span>
     </div>
   )
